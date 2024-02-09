1fe8644204d3d23001a8d8fe804e440a53e90f35
William Kasasa <william.kasasa@student.kdg.be>
2024-02-09 14:51:37 +0100

    Initial commit

---
+++ src/main/java/be/kdg/programming3/projectwilliamkasasa/ProjectWilliamKasasaApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.programming3.projectwilliamkasasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWilliamKasasaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWilliamKasasaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/kdg/programming3/projectwilliamkasasa/ProjectWilliamKasasaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.programming3.projectwilliamkasasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWilliamKasasaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
